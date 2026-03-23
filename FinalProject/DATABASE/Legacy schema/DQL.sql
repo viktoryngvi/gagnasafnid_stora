bbf9fef590d997288d368333010611310ec93e30
JBT13 <jeremiahborjas@gmail.com>
2026-03-23 13:18:17 +0000

    Done? With the first query

--- FinalProject/DATABASE/Legacy schema/DQL.sql
+++ FinalProject/DATABASE/Legacy schema/DQL.sql
@@ -1,1 +1,32 @@
 -- Task A2
+SELECT *
+FROM raforka_legacy.orku_einingar
+LIMIT 20;
+
+SELECT *
+FROM raforka_legacy.orku_einingar_id_seq
+
+SELECT *
+FROM raforka_legacy.orku_maelingar
+LIMIT 20;
+
+SELECT *
+FROM raforka_legacy.orku_maelingar_id_seq
+
+SELECT *
+FROM raforka_legacy.orku_maelingar_pkey
+
+SELECT 
+    M.eining_heiti AS "Power_Plant_Source",
+    EXTRACT(YEAR FROM timi) AS "Year",
+    EXTRACT(MONTH FROM timi) AS "Month",
+    M.tegund_maelingar "Type",
+    SUM(M.gildi_kwh) AS "Total_kWh"
+FROM raforka_legacy.orku_maelingar M
+WHERE EXTRACT(YEAR FROM timi) = 2025
+GROUP BY 
+    M.eining_heiti,
+    "Year",
+    "Month",
+    M.tegund_maelingar
+LIMIT 50;
