87dcb3ee2b5291bb40032129cffc340c0f2c4390
viktoryngvi <viktoryngvi@gmail.com>
2026-03-25 18:28:53 +0000

    næstum búið migration

--- FinalProject/DATABASE/Updated schema/migrate_data.py
+++ FinalProject/DATABASE/Updated schema/migrate_data.py
@@ -1,5 +1,6 @@
 # Task C4
 import psycopg2
+from datetime import date
 
 
 #testing and format
@@ -11,73 +12,62 @@ user="postgres",
 password="123"    #má ekki vera tómt
 ); print("Connected!")
 
-# seconddb = psycopg2.connect(
-# host="localhost",
-# port=5432,
-# dbname="new_db",   #þurfið að breyta í ykkar
-# user="postgres",
-# password="123"    #má ekki vera tómt
-# ); print("Connected!")
-
-
-
 cur = fyrstdb.cursor()
+cur.execute("CREATE DATABASE new_improved;")
+
+seconddb = psycopg2.connect(
+host="localhost",
+port=5432,
+dbname="new_improved",   #þurfið að breyta í ykkar
+user="postgres",
+password="123"    #má ekki vera tómt
+); print("Connected!")
+
+
+
 sec = seconddb.cursor()
+
 
 cur.execute("SELECT * FROM raforka_legacy.orku_einingar;")
 for i in cur:
-    sec.execute(
-        f"INSERT INTO orku_stodvar_eigandi (heiti_eigandans) VALUES ({i[4]})"
-        f"INSERT INTO hnit (X_HNIT, Y_HNIT) VALUES ({i[8]}, {i[9]})"  ##þarf SERIAL eða eih til að iteratea nothæf ID's
-        f"INSERT INTO stod (stadur, dagsetning_uppsett, hvort_dat_se_stod, tegund)"
-        f"VALUES ({i[1]}, {i[5]}-{("0"+i[6]) if i < 10 else i}-{("0"+i[7]) if i < 10 else i}, {i[2]}, {i[3]})"
-    )
+    sec.execute("INSERT INTO orku_stodvar_eigandi (heiti_eigandans) VALUES (%s);", (i[4],))
+    sec.execute("INSERT INTO hnit (X_HNIT, Y_HNIT) VALUES (%s, %s);", (i[8], i[9]))
+
+    y = i[5]; m = i[6]; d = i[7]
+    whole = date(y, m, d)
+
+    sec.execute("INSERT INTO stod (hnit_id, stadur, dagsetning_uppsett, hvort_dat_se_stod, tegund) "
+                "VALUES ((SELECT ID FROM hnit WHERE X_hnit = %s AND Y_HNIT = %s), "
+                "%s, %s, %s, %s);", (i[8], i[9], i[1], whole, i[2], i[3]))
     if i[10] is not None:
         sec.execute(
-            f"INSERT INTO tengdar_stodvar (STOD1, STOD2) VALUES ({i[1]}, {i[10]})"
-        )   ###þarf kannski að breyta yfir í nafn vegna hvernig þetta initializast
+            "INSERT INTO tengdar_stodvar (STOD1, STOD2) VALUES (%s, %s);", (i[1], i[10]))
+            ###þarf kannski að breyta yfir í nafn vegna hvernig þetta initializast
             ###eða að stofna fyrst allar stöðvarnar áður en við gerum tengdar stöðvar
 
-
-cur.execute("SELECT * FROM raforka_legacy.notendur_skraning;")
+with fyrstdb:
+    cur.execute("SELECT * FROM raforka_legacy.notendur_skraning;")
 
 for i in cur:
-    sec.execute(
-        f"INSERT INTO hnit (X_HNIT, Y_HNIT) VALUES ({i[5]}, {i[6]})"
-        f"INSERT INTO notandi (hnit_id, stadsetning, kennitala, eigandi, ar_stofnad)"
-        f"VALUES ({hnit_id}, {eigandi_id}, {i{1}}, {i[2]}, {i[3]}, {i[4]})"
-    )
+    sec.execute("INSERT INTO hnit (X_HNIT, Y_HNIT) VALUES (%s, %s);", (i[5], i[6]))
+    sec.execute("INSERT INTO notandi (hnit_id, stadsetning, kennitala, eigandi, ar_stofnad)"
+        "VALUES ((SELECT ID FROM hnit WHERE X_HNIT = %s AND Y_HNIT = %s), "
+        "%s, %s, %s, %s);", (i[5], i[6], i[1], i[2], i[3], i[4]))
 
-while 
-cur.intersize = 20000
-cur.execute("SELECT * FROM raforka_legacy.orku_maelingar;")
-for i in cur:
+with fyrstdb:
+    cur.intersize = 20000
+    cur.execute("SELECT * FROM raforka_legacy.orku_maelingar;")
 
-    sec.execute(
-        f"INSERT INTO orku_maelingar (ID, STOD_ID, notandi, date_og_timi, tegund_maelingar, gildi_kwh)"
-        f"VALUES ({i[0]}, {STOD_ID},)"
-    )
-
-
-
-with connection:
-
-    cursor = connection.cursor()
-    with cursor:
-        cursor.itersize = 20000
-        cursor.execute("SELECT malware_id, malwarehashmd5, malwarehashsha1, malwarehashsha256g FROM malwarehashesandstrings")
-        listoffetchedmalware = cursor.fetchall()
-
-
-        listofmalwareobjects = processrecords(listoffetchedmalware)
-
-
-
-
-
-
-
-
+    for i in cur:
+        with seconddb:
+            sec.execute("INSERT INTO maelingar (STOD_ID, sendandi_maelingar, tegund_maelingar, timi)"
+                "VALUES ((SELECT ID FROM stod WHERE eining_heiti = %s), %s, %s, "
+                "%s;", (i[1], i[3], i[2], i))
+            sec.execute("INSERT INTO gildin (maeling_ID, notandi_ID, gildi_kwh)"
+                        "VALUES "
+                        "(SELECT ID FROM maeling WHERE ID = %s)"
+                        "(SELECT ID FROM notandi WHERE notandi_heiti = %s)"
+                        "%s;", (i[0], i[3], i[5], i[4]))
 
 
 
@@ -94,7 +84,15 @@ with connection:
 
 
 cur = fyrstdb.cursor()
-cur.execute("SELECT * FROM raforka_legacy.notendur_skraning;")
+cur.execute("SELECT * FROM raforka_legacy.orku_einingar LIMIT 60;")
+
+
+
+
+
+
+# cur = fyrstdb.cursor()
+# cur.execute("SELECT * FROM raforka_legacy.orku_maelingar LIMIT 5;")
 
 for i in cur:
     print(i)
