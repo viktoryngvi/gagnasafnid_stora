92c4ddcd09f8ccbb785e00e10d94bee863c9e48b
viktoryngvi <viktoryngvi@gmail.com>
2026-03-26 12:56:08 +0000

    nytt

--- FinalProject/DATABASE/Updated schema/migrate_data.py
+++ FinalProject/DATABASE/Updated schema/migrate_data.py
@@ -9,7 +9,7 @@ host="localhost",
 port=5432,
 dbname="final",   #Þurfið að breyta í ykkar
 user="postgres",
-password=""    #Er ykkar password
+password="123"    #Er ykkar password
 ); print("Connected!")
 
 fyrstdb.autocommit = True
@@ -22,8 +22,8 @@ host="localhost",
 port=5432,
 dbname="new_improved",
 user="postgres",
-password=""    #Er ykkar password
-); print("Connected!")
+password="123"    #Er ykkar password
+); print("Starting to run, wait a moment")
 
 sec = seconddb.cursor()
 
@@ -34,15 +34,18 @@ sec.execute("CREATE TABLE virkjun (ID INT PRIMARY KEY REFERENCES orku(ID), tegun
 sec.execute("CREATE TABLE stod (ID INT PRIMARY KEY REFERENCES orku(ID), tegund VARCHAR NOT NULL);")
 sec.execute("CREATE TABLE tengdar_stodvar (STOD1 VARCHAR, STOD2 VARCHAR, PRIMARY KEY(STOD1, STOD2), FOREIGN KEY(STOD1) REFERENCES orku(stadur), FOREIGN KEY(STOD2) REFERENCES orku(stadur));")
 sec.execute("CREATE TABLE notandi (ID SERIAL PRIMARY KEY, hnit_id INT, stadsetning VARCHAR NOT NULL, kennitala VARCHAR NOT NULL UNIQUE, eigandi VARCHAR NOT NULL, ar_stofnad VARCHAR NOT NULL, FOREIGN KEY(hnit_id) REFERENCES hnit(ID));")
-sec.execute("CREATE TABLE maeling ( ID SERIAL PRIMARY KEY, orku_ID INT, sendandi_maelingar VARCHAR NOT NULL, tegund_maelingar VARCHAR NOT NULL, timi TIMESTAMP NOT NULL, FOREIGN KEY (orku_ID) REFERENCES orku(ID));")
-sec.execute("CREATE TABLE gildin (ID SERIAL PRIMARY KEY, maeling_ID INT, notandi_ID INT NULL, gildi_kwh NUMERIC NOT NULL, FOREIGN KEY (notandi_ID) REFERENCES notandi(ID), FOREIGN KEY (maeling_ID) REFERENCES maeling(ID));")
+sec.execute("CREATE TABLE maeling ( ID SERIAL PRIMARY KEY, orku_ID INT, sendandi_maelingar VARCHAR NOT NULL, tegund_maelingar VARCHAR NOT NULL, FOREIGN KEY (orku_ID) REFERENCES orku(ID));")
+sec.execute("CREATE TABLE gildin (ID SERIAL PRIMARY KEY, maeling_ID INT, notandi_ID INT NULL, gildi_kwh NUMERIC NOT NULL, timi TIMESTAMP NOT NULL, FOREIGN KEY (notandi_ID) REFERENCES notandi(ID), FOREIGN KEY (maeling_ID) REFERENCES maeling(ID));")
 
 with fyrstdb:
     cur.itersize = 20000
     cur.execute("SELECT * FROM raforka_legacy.orku_einingar;")
+    listinn = []
     for i in cur:
 
         with seconddb:
+
+            if i[4] is in listinn
             sec.execute("INSERT INTO orku_stodvar_eigandi (heiti_eigandans) VALUES (%s);", (i[4],))
             sec.execute("INSERT INTO hnit (X_HNIT, Y_HNIT) VALUES (%s, %s);", (i[8], i[9]))
 
@@ -60,10 +63,13 @@ with fyrstdb:
             if i[2] == "virkjun":
                 sec.execute("INSERT INTO virkjun (ID, tegund)"
                         "VALUES ((SELECT ID FROM orku WHERE stadur = %s), %s);", (i[1], i[2]))
+                
+    cur.itersize = 20000
+    cur.execute("SELECT heiti, tengd_stod FROM raforka_legacy.orku_einingar;")
     for i in cur:
         with seconddb:
-            if i[10] is not None:
-                sec.execute("INSERT INTO tengdar_stodvar (STOD1, STOD2) VALUES (%s, %s);", (i[1], i[10]))
+            if i[1] is not None:
+                sec.execute("INSERT INTO tengdar_stodvar (STOD1, STOD2) VALUES (%s, %s);", (i[0], i[1]))
 
 with fyrstdb:
     cur.itersize = 20000
@@ -82,18 +88,32 @@ with fyrstdb:
     for i in cur:
 
         with seconddb:
-            sec.execute("INSERT INTO maeling (orku_ID, sendandi_maelingar, tegund_maelingar, timi)"
-                "VALUES ((SELECT ID FROM orku WHERE stadur = %s), %s, %s, %s);", 
-                (i[1], i[3], i[2], i[4]))
+            sec.execute("INSERT INTO maeling (orku_ID, sendandi_maelingar, tegund_maelingar)"
+                "VALUES ((SELECT ID FROM orku WHERE stadur = %s), %s, %s);", 
+                (i[1], i[3], i[2]))
             if i[6] is not None:
-                sec.execute("INSERT INTO gildin (maeling_ID, gildi_kwh, notandi_ID) VALUES ("
+                sec.execute("INSERT INTO gildin (maeling_ID, gildi_kwh, notandi_ID, timi) VALUES ("
                     "(SELECT ID FROM maeling WHERE ID = %s), %s, " 
-                    "(SELECT ID FROM notandi WHERE stadsetning = %s));", 
-                    (i[0], i[5], i[6]))
+                    "(SELECT ID FROM notandi WHERE eigandi = %s), %s);", 
+                    (i[0], i[5], i[6], i[4]))
             else:
-                sec.execute("INSERT INTO gildin (maeling_ID, gildi_kwh)"
+                sec.execute("INSERT INTO gildin (maeling_ID, gildi_kwh, timi)"
                         "VALUES ("
                         "(SELECT ID FROM maeling WHERE ID = %s), %s);", 
                         (i[0], i[5]))
 
 print("Geggjað, vírusinn er kominn:)")
+
+
+cur = fyrstdb.cursor()
+cur.execute("SELECT * FROM raforka_legacy.orku_einingar LIMIT 100;")
+
+
+for i in cur:
+    print(i)
+
+
+
+
+
+
--- FinalProject/DATABASE/Updated schema/tests.sql
+++ FinalProject/DATABASE/Updated schema/tests.sql
@@ -1,2 +1,10 @@
+
+
+
 SELECT *
-FROM orku;
+FROM raforka_legacy.orku_maelingar
+LIMIT 200;
+
+-- viljum ekki fá eining heiti, tegund maelingar og sendandi maelnggar oft upp
+-- skipt tíma yfir á gildi
+-- má ekki vera landsnet duplicates í orku_stöðvar_eigandi
