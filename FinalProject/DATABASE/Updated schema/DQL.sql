d781a110cbf420a9abba0caa2f1c9c654a100790
viktoryngvi <viktoryngvi@gmail.com>
2026-03-30 13:05:50 +0000

    querries

--- FinalProject/DATABASE/Updated schema/DQL.sql
+++ FinalProject/DATABASE/Updated schema/DQL.sql
@@ -3,14 +3,14 @@
 -- 1. Query 
 SELECT 
     S.stadur "Power_Plant_Source",
-    EXTRACT(YEAR FROM M.timi) AS "Year",
-    EXTRACT(MONTH FROM M.timi) AS "Month",
+    EXTRACT(YEAR FROM G.timi) AS "Year",
+    EXTRACT(MONTH FROM G.timi) AS "Month",
     M.tegund_maelingar AS "Type",
     SUM(G.gildi_kwh) AS "Total_kWh"
 FROM gildin G
 JOIN maeling M ON M.ID = G.maeling_ID
-JOIN stod S on S.ID = M.stod_ID
-WHERE EXTRACT(YEAR FROM M.timi) = 2025
+JOIN orku S on S.ID = M.orku_ID
+WHERE EXTRACT(YEAR FROM G.timi) = 2025
 GROUP BY
     S.stadur,
     "Year",
@@ -25,15 +25,15 @@ ORDER BY
 -- 2. Query SEMI SEMI 
 SELECT 
     S.stadur "Power_Plant_Source",
-    EXTRACT(YEAR FROM M.timi) AS "Year",
-    EXTRACT(MONTH FROM M.timi) AS "Month",
+    EXTRACT(YEAR FROM G.timi) AS "Year",
+    EXTRACT(MONTH FROM G.timi) AS "Month",
     N.eigandi AS "Customer_name",
     SUM(G.gildi_kwh) AS "Total_kWh"
 FROM gildin G
 JOIN maeling M ON M.ID = G.maeling_ID
 JOIN notandi N ON N.ID = G.notandi_ID
-JOIN stod S ON S.ID = M.stod_ID
-WHERE EXTRACT(YEAR FROM M.timi) = 2025
+JOIN orku S ON S.ID = M.stod_ID
+WHERE EXTRACT(YEAR FROM G.timi) = 2025
 GROUP BY
     S.stadur,
     "Year",
@@ -67,10 +67,10 @@ SELECT
     ) AS "Total_System_Loss_Ratio"
 FROM gildin G
 JOIN maeling M ON M.ID = G.maeling_ID
-JOIN stod S ON S.ID = M.stod_ID
+JOIN orku S ON S.ID = M.orku_ID
 GROUP BY 
-    s.stadur,
-    EXTRACT(MONTH FROM M.timi)
+    S.stadur,
+    EXTRACT(MONTH FROM G.timi)
 
 
 SELECT "Power_Plant_Source", AVG("Plant_To_Sub_loss_Ratio"), AVG("Total_System_Loss_Ratio")
