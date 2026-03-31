5f047e38de7f347ecefdc905710e793644d7c1c2
DanielDarri <danieldarri2005@gmail.com>
2026-03-31 17:22:29 +0000

    lessssgó F APi ready

--- FinalProject/API/Updated api/app/routes/endpoints.py
+++ FinalProject/API/Updated api/app/routes/endpoints.py
@@ -7,6 +7,7 @@ from app.services.service import (
     get_monthly_energy_flow_data,
     get_monthly_company_usage_data,
     get_monthly_plant_loss_ratios_data,
+    get_substations_gridflow_data
 )
 from app.utils.validate_date_range import validate_date_range_helper
 from datetime import datetime
@@ -68,3 +69,17 @@ Endpoint 4: insert_measurements()
 '''
 Endpoint 5: get_substations_gridflow()
 '''
+@router.get("/substation-gridflow")
+def get_substations_gridflow(
+    from_date: datetime | None = None,
+    to_date: datetime | None = None,
+    db: Session = Depends(get_orkuflaedi_session)
+):
+    print(f"Calling [GET] /{db_name}/substation-gridflow")
+    from_date, to_date = validate_date_range_helper(
+        from_date,
+        to_date,
+        datetime(2025, 1, 1, 0, 0),
+        datetime(2026, 1, 1, 0, 0)
+    )
+    return get_substations_gridflow_data(db, from_date, to_date)
